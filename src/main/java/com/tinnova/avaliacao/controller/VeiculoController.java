c2146cf76d949af5bca8b8f4aad5c580394c37ce
wdeus <wagner.silva96@fatec.sp.gov.br>
2023-04-02 15:19:32 -0300

    Validação para não permitir cadastro de marcas com nome errado

--- src/main/java/com/tinnova/avaliacao/controller/VeiculoController.java
+++ src/main/java/com/tinnova/avaliacao/controller/VeiculoController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -52,8 +53,17 @@ public class VeiculoController {
 	
 	@PostMapping
 	@Transactional
-	public void cadastrar(@RequestBody DadosVeiculo dadosVeiculo) {
-		repository.save(new Veiculo(dadosVeiculo));
+	public ResponseEntity cadastrar(@RequestBody DadosVeiculo dadosVeiculo) {
+		try {
+			Veiculo veiculo = new Veiculo();
+			veiculo = service.salvarVeiculo(veiculo, dadosVeiculo);
+			repository.save(veiculo);
+			return ResponseEntity.ok(veiculo);
+		} catch(IllegalArgumentException e) {
+			e.printStackTrace();
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		
 	}
 	
 	@PatchMapping("/{id}")
--- src/main/java/com/tinnova/avaliacao/service/VeiculoService.java
+++ src/main/java/com/tinnova/avaliacao/service/VeiculoService.java
@@ -2,6 +2,8 @@ package com.tinnova.avaliacao.service;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import com.tinnova.avaliacao.dto.DadosAtualizacaoVeiculo;
 import com.tinnova.avaliacao.dto.DadosVeiculo;
@@ -9,6 +11,8 @@ import com.tinnova.avaliacao.model.Veiculo;
 
 public class VeiculoService {
 
+	private List<String> marcas = Arrays.asList("MERCEDES", "FORD", "CITROEN", "BMW", "CHEVROLET", "VOLKSWAGEN");
+	
 	public Veiculo atualizarVeiculo(Veiculo veiculo, DadosVeiculo dadosVeiculo) {
 		veiculo.setVeiculo(dadosVeiculo.veiculo());
 		veiculo.setMarca(dadosVeiculo.marca());
@@ -23,5 +27,22 @@ public class VeiculoService {
 		veiculo.setMarca(dadosVeiculo.marca());
 		veiculo.setAno(dadosVeiculo.ano());
 		return veiculo;
+	}
+	
+	public Veiculo salvarVeiculo(Veiculo veiculo, DadosVeiculo dadosVeiculo) {
+		veiculo.setVeiculo(dadosVeiculo.veiculo());
+		veiculo.setMarca(verificaMarcaValida(dadosVeiculo.marca()));
+		veiculo.setAno(dadosVeiculo.ano());
+		veiculo.setDescricao(dadosVeiculo.descricao());
+		veiculo.setVendido(dadosVeiculo.vendido());
+		veiculo.setCreated(Date.valueOf(LocalDate.now()));
+		veiculo.setUpdated(Date.valueOf(LocalDate.now()));
+		return veiculo;
+	}
+	
+	private String verificaMarcaValida(String marca) {
+		if(!marcas.contains(marca.toUpperCase())) 
+			throw new IllegalArgumentException("Marca inválida");	
+		return marca;
 	}
 }
